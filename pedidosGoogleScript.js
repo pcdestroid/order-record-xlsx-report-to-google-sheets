237b8fdb423eef00d1e7ab2e860ecfe045016ddf
Alan Francis <60854121+pcdestroid@users.noreply.github.com>
2022-08-27 17:28:11 -0300

    Update pedidosGoogleScript.js
    alguns ajustes

--- pedidosGoogleScript.js
+++ pedidosGoogleScript.js
@@ -1,30 +1,30 @@
 //Converter o Relatório 301 em arquivo google planilha
 async function converterExcelParaGoogleSheets() {
   let nome = 'SCSC301'
-  var idPastaOrigem = ""; // id da pasta onde esta o arquivo xlsx
-  var idPastaDestino = ""; // id da pasta onde irá salvar o arquivo google sheets
+  var idPastaOrigem = "1HX8E1CnWYyavLU6bkNQZaBWmSLT8YQH9";
+  var idPastaDestino = "1znhgZ80zGTiJTLg0rHEJGhxUJzgPNqM8";
   var pastaDestino = DriveApp.getFolderById(idPastaDestino)
   var arquivos = pastaDestino.getFiles()
   var arquivo = arquivos.next()
   var id = arquivo.getId()
   Logger.log(id)
-  if  (arquivo == nome) {
-  DriveApp.getFolderById(id).setTrashed(true);
+  if (arquivo == nome) {
+    DriveApp.getFolderById(id).setTrashed(true);
   }
 
   var files = DriveApp.getFolderById(idPastaOrigem).getFilesByType(MimeType.MICROSOFT_EXCEL);
-  
-  while(files.hasNext()){
+
+  while (files.hasNext()) {
     var file = files.next();
-    var name = file.getName().split('.')[0]; 
+    var name = file.getName().split('.')[0];
     var id = file.getId();
     var blob = file.getBlob();
- 
+
     var newFile = {
-        title : name,
-        parents: [{id: idPastaDestino}] 
-      }; 
-      
+      title: name,
+      parents: [{ id: idPastaDestino }]
+    };
+
     var sheetFile = Drive.Files.insert(newFile, blob, { convert: true });
   }
 
@@ -33,8 +33,8 @@ async function converterExcelParaGoogleSheets() {
   arquivo = arquivos.next()
   id = arquivo.getId()
   Logger.log(id)
-  if  (arquivo == nome) {
-  return id
+  if (arquivo == nome) {
+    return id
   }
 
 }
@@ -46,7 +46,8 @@ function indiceColuna(x, y, z) {
 }
 
 async function pesquisarPedido() {
-  var idPlan = await converterExcelParaGoogleSheets()
+  //var idPlan = await converterExcelParaGoogleSheets()
+  var idPlan = '1KnYiT7oLS4KEcmD96UnANIxVgIvNR9fYtM5pcXRwu7k'
   var base = SpreadsheetApp.openById(idPlan)
   var guiaDados = base.getSheetByName('Plan1');
   var pedidos = SpreadsheetApp.getActiveSpreadsheet();
@@ -70,18 +71,14 @@ async function pesquisarPedido() {
   var proxProd = 5
   var x = 0
   for (var linha = 0; linha < dados.length; linha++) {
-
     if (dados[linha][0] + dados[linha][1] == pesquisa) {
-
       x = 1
-
-      let empresa = dados[linha][0]
-      let sc = dados[linha][1]
-      let seq = dados[linha][3]
-      let produto = dados[linha][4]
-      let descricao = dados[linha][5]
-      let obs = dados[linha][8]
-      let um = dados[linha][13]
+      
+      let empresa = dados[linha][0],sc = dados[linha][1],seq = dados[linha][3],produto = dados[linha][4],descricao = dados[linha][5],obs = dados[linha][8],um = dados[linha][13];
+      let familia = dados[linha][14],quantidade = dados[linha][15],dataSolicitacao = dados[linha][16],solicitanteSc = dados[linha][18],aprovadorSc = dados[linha][20],sitAprovSc = dados[linha][21];
+      let dataAprovSc = dados[linha][22],nroOc = dados[linha][25],statusOc = dados[linha][26],seqOc = dados[linha][27],codForn = dados[linha][28],descricaoFornecedor = dados[linha][29];
+      let qtdOc = dados[linha][31],valorOc = dados[linha][32],dataOc = dados[linha][33],usuarioGerouOc = dados[linha][36],sitAprovOc = dados[linha][38],aprovadorOc = dados[linha][41];
+      let dataAprovOc = dados[linha][42]
 
       guiamenu.getRange(proxProd, indiceColuna('Empresa', 4, guiamenu)).setValue(empresa)
       guiamenu.getRange(proxProd, indiceColuna('Solicitação', 4, guiamenu)).setValue(sc)
@@ -90,6 +87,25 @@ async function pesquisarPedido() {
       guiamenu.getRange(proxProd, indiceColuna('Descrição', 4, guiamenu)).setValue(descricao)
       guiamenu.getRange(proxProd, indiceColuna('Observação', 4, guiamenu)).setValue(obs)
       guiamenu.getRange(proxProd, indiceColuna('U.M', 4, guiamenu)).setValue(um)
+      guiamenu.getRange(proxProd, indiceColuna('Família', 4, guiamenu)).setValue(familia)
+      guiamenu.getRange(proxProd, indiceColuna('Quantidade', 4, guiamenu)).setValue(quantidade)
+      guiamenu.getRange(proxProd, indiceColuna('Data Solic.', 4, guiamenu)).setValue(dataSolicitacao)
+      guiamenu.getRange(proxProd, indiceColuna('Solicitante SC', 4, guiamenu)).setValue(solicitanteSc)
+      guiamenu.getRange(proxProd, indiceColuna('Aprovador SC', 4, guiamenu)).setValue(aprovadorSc)
+      guiamenu.getRange(proxProd, indiceColuna('Sit. Aprov.', 4, guiamenu)).setValue(sitAprovSc)
+      guiamenu.getRange(proxProd, indiceColuna('Data  Aprov.', 4, guiamenu)).setValue(dataAprovSc)
+      guiamenu.getRange(proxProd, indiceColuna('Nro OC', 4, guiamenu)).setValue(nroOc)
+      guiamenu.getRange(proxProd, indiceColuna('Status da OC', 4, guiamenu)).setValue(statusOc)
+      guiamenu.getRange(proxProd, indiceColuna('Seq. OC', 4, guiamenu)).setValue(seqOc)
+      guiamenu.getRange(proxProd, indiceColuna('Cod. Fornec.', 4, guiamenu)).setValue(codForn)
+      guiamenu.getRange(proxProd, indiceColuna('Descrição Fornecedor', 4, guiamenu)).setValue(descricaoFornecedor)
+      guiamenu.getRange(proxProd, indiceColuna('Qtd. OC', 4, guiamenu)).setValue(qtdOc)
+      guiamenu.getRange(proxProd, indiceColuna('Valor', 4, guiamenu)).setValue(valorOc)
+      guiamenu.getRange(proxProd, indiceColuna('Data OC', 4, guiamenu)).setValue(dataOc)
+      guiamenu.getRange(proxProd, indiceColuna('Usuário Gerou OC', 4, guiamenu)).setValue(usuarioGerouOc)
+      guiamenu.getRange(proxProd, indiceColuna('Sit. Aprov. OC', 4, guiamenu)).setValue(sitAprovOc)
+      guiamenu.getRange(proxProd, indiceColuna('Aprovador OC', 4, guiamenu)).setValue(aprovadorOc)
+      guiamenu.getRange(proxProd, indiceColuna('Data Aprov. OC', 4, guiamenu)).setValue(dataAprovOc)
 
       proxProd++
 
